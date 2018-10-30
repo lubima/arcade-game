6fbb4f6f116e16586c40673c291e46a6fb0c5255
Lyubima Toshkova <lyubimatoshkova18@gmail.com>
2018-10-30 16:33:14 -0500

    feat:Player moves within the playground respecting the boundaries

--- js/app.js
+++ js/app.js
@@ -39,28 +39,55 @@ class Enemy {
 //             Sprite Image    
 class Player {
     constructor(){
-        this.x = 0;
-        this.y = 0;
         this.sprite = 'images/char-horn-girl.png';
-    }
+        this.step = 101;
+        this.jump = 83;
+        this.startX = this.step * 2;
+        this.startY = (this.jump * 5) - 20;
+        this.x = this.startX;
+        this.y = this.startY;
+        }
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
+    handleInput(input) {
+        switch(input){
+            case 'left':
+                if(this.x > 0){
+                    this.x -= this.step;
+                }
+                break;
+            case 'up':
+                if(this.y > this.jump){
+                    this.y -= this.jump;
+                }
+                break;
+            case 'right':
+                if(this.x < this.step * 4){
+                    this.x += this.step;
+                }
+                break;
+            case 'down':
+                if(this.y < this.jump * 4){
+                    this.y += this.jump;
+                }
+                break;
+        }
+    }
 }
 
-//         Methods
-                // Update position
-                    // Check collision here
-                        //Did player x and y collide with enemy?
-                    // Check win here?
-                        // Did player x and y reach final tile?
-                // Render
-                    // Draw player sprite on current x and y coord position
-                    
-                // Handle keyboard input
-                    // Update player's x and y property according to input
-                // Reset Player
-                    // Set x and y to starting x and y 
+//Methods
+    // Update position
+        // Check collision here
+            //Did player x and y collide with enemy?
+        // Check win here?
+            // Did player x and y reach final tile?
+    // Render
+        // Draw player sprite on current x and y coord position
+    // Handle keyboard input
+        // Update player's x and y property according to input
+    // Reset Player
+        // Set x and y to starting x and y 
 
 // Now write your own player class
 // This class requires an update(), render() and
